fae660d816eb56a5b10cb97793b634444ab9810d
Jason MacWilliams <jason@discoverygarden.ca>
2012-03-26 15:26:43 -0300

    Fixed navigation scheme

--- Navigator.py
+++ Navigator.py
@@ -3,6 +3,10 @@ from utils.commonFedora import *
 import glob, zipfile, sys
 import FileIngester
 
+def get_immediate_subdirectories(dir):
+    return [name for name in os.listdir(dir)
+        if os.path.isdir(os.path.join(dir, name))]
+
 """ ====== SCAN FOR OBJECTS IN A FOLDER ====== """
 def processFolder(fedora, config):
     """
@@ -22,16 +26,13 @@ def processFolder(fedora, config):
     myCollection = addCollectionToFedora(fedora, config.myCollectionName, myPid=config.myCollectionPid, parentPid=config.hostCollectionPid, tnUrl=config.myCollectionIcon)
 
     # this is the list of all folders to search in for books
-    baseFileDict = { 'parentPid' : config.myCollectionPid, 'contentModel' : 'islandora:bookCModel' }
+    baseFileDict = { 'parentPid' : config.myCollectionPid, 'contentModel' : 'islandora:mapCModel' }
     totalFiles = 0
     completeFiles = 0
     for subFolder in os.listdir(folder):
         if os.path.isdir(os.path.join(folder, subFolder)):
 
             print("Scan Folder %s" % subFolder)
-            # the single item trap - very helpful for testing
-            #if subFolder != "00awa1617m":
-            #    continue
 
             fileDict = { 'label': subFolder, 'datastreams' : { } }
 
@@ -42,13 +43,14 @@ def processFolder(fedora, config):
                     return True
                 return False
 
+            if not addFileByPattern("TIFF", "*.tif"):
+                if not addFileByPattern("TIFF", "*.tiff"):
+                    # failed
+                    print("Could not find base tif file - skipping directory %s" % subFolder)
+                    continue # next subFolder
             addFileByPattern("MODS", "*.mods.xml")
             addFileByPattern("DC", "*.dc.xml")
             addFileByPattern("FGDC", "*.fgdc.xml")
-            addFileByPattern("CNTRLP", "*/*.controlpts.txt")
-            addFileByPattern("TFWX", "*/*.tfwx")
-            addFileByPattern("AUX", "*/*.aux")
-            addFileByPattern("RRD", "*/*.rrd")
 
             # creation of the dictionary here might be bad
             fileDict.update(baseFileDict)
@@ -56,5 +58,24 @@ def processFolder(fedora, config):
             if FileIngester.createObjectFromFiles(fedora, config, fileDict):
                 print("Object (%s) ingested successfully" % subFolder)
                 completeFiles = completeFiles + 1
+
+                # now ingest the georef clips
+                for georefclip in get_immediate_subdirectories(os.path.join(folder, subFolder)):
+                    # clear the datastreams list for the georef clips
+                    subFileDict = { 'label': georefclip, 'datastreams' : {} }
+                    # georef clip metadata
+                    # maybe perform a subloop on these after object creation?
+                    if not addFileByPattern("TIFF", "%s/*.tif" % georefclip):
+                        if not addFileByPattern("TIFF", "%s/*.tiff" % georefclip):
+                            # failed
+                            print("Could not find base tif file - skipping georefclip directory %s" % georefclip)
+                            continue # next subFolder
+                    addFileByPattern("CNTRLP", "%s/*.controlpts.txt" % georefclip)
+                    addFileByPattern("CNTRLPXML", "%s/*.controlpts.txt.xml" % georefclip)
+                    addFileByPattern("TFWX", "%s/*.tfwx" % georefclip)
+                    addFileByPattern("AUX", "%s/*.aux" % georefclip)
+                    addFileByPattern("RRD", "%s/*.rrd" % georefclip)
+                    addFileByPattern("TIFXML", "%s/*.tif.xml" % georefclip)
+                    FileIngester.createObjectFromFiles(fedora, config, fileDict)
 
     return completeFiles
